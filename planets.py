24f95ba131eb6b1034c9f68827d273f4aaa45f32
mmcdevitt1997 <30372557+mmcdevitt1997@users.noreply.github.com>
2019-08-26 16:52:39 -0500

    planets being looped over

--- planets.py
+++ planets.py
@@ -2,11 +2,19 @@ planet_list = ["Mercury", "Mars"]
 
 planet_list.append("Jupiter")
 planet_list.append("Saturn")
-planet_list.extend (["Neptune", "Uranus"])
+planet_list.extend(["Neptune", "Uranus"])
 planet_list.insert(1, "Venus")
 planet_list.insert(2, "Earth")
 planet_list.append("Pluto")
 del planet_list[8]
 
-rocky_planets = slice(0,4)
-print (planet_list[rocky_planets])
+rocky_planets = slice(0, 4)
+spacecraft = [
+    ("Cassini", "Saturn"), ("Viking", "Mars"), ("Alex", "Jupiter"), ("Boy", "Pluto")
+]
+
+for (value, key) in spacecraft:
+    print(f"The spacecraft {value} has been to {key}")
+
+
+# print(planet_list[rocky_planets])
